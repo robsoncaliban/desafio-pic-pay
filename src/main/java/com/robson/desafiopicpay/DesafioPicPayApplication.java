1e6213021e708451035489af4b35927a8c9585b2
robsoncaliban <robsoncaliban@gmail.com>
2024-11-01 22:31:01 -0300

    docs: Adiciona metadados da api com @OpenAPIDefinition na classe principal

--- src/main/java/com/robson/desafiopicpay/DesafioPicPayApplication.java
+++ src/main/java/com/robson/desafiopicpay/DesafioPicPayApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@OpenAPIDefinition(info = @Info(title = "Desafio Pic-Pay", version = "1", description = "API desenvolvida para fazer transações entre usuarios"))
 public class DesafioPicPayApplication {
 
 	public static void main(String[] args) {
